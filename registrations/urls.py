85e53465c586e123447aaa7bca8b56e97733b52d
Jerwin Wieser <j2.wieser@gmail.com>
2020-08-20 22:39:21 +0200

    init

--- registrations/urls.py
+++ registrations/urls.py
@@ -13,18 +13,3 @@ urlpatterns = [
 
 
 
-
-
-
-
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#  	# path('rest/<int:pk>/', views.data_rest, name='data_rest'),
-#  	path('chart/', views.render_chart, name='chart'),
-#  	path('form/', views.render_form, name='form'),
-#  	path('delete/<person_id>', views.delete, name='delete'),
-#  	path('edit/<person_id>', views.edit, name='edit'),
-# ]
-
-
-
