b5fed1a83bb18382160ac8d6ddef66e717ace364
Jerwin Wieser <j2.wieser@gmail.com>
2020-08-20 22:31:12 +0200

    init

--- registrations/views.py
+++ registrations/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect	
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import User, Permission
