a5770d9d470dcc1892f90728572a418cebc9ea7c
Jerwin Wieser <j2.wieser@gmail.com>
2020-08-21 11:08:42 +0200

    chart fix

--- registrations/views.py
+++ registrations/views.py
@@ -21,32 +21,24 @@ import pandas
 
 @api_view(['GET'])
 def data_rest_api(request):
-	persons = Person.objects.all()
 	persons_by_user = Person.objects.filter(created_by_id=request.user)
-	
 	serializer = PersonSerializer(persons_by_user, many=True)
-	
-	df = pandas.DataFrame(list(persons_by_user.values('name', 'duration', 'created_at', 'updated_at', 'created_by_id')))
 
-	df = df.groupby('duration').count()
+	names = [item for item in persons_by_user.values_list('name', flat=True)]
+	person_age = [item for item in persons_by_user.values_list('person_age', flat=True)]
 
 	data = {
-		'x': df.index.values,
-		'y': df.name.values,
+		'x': names,
+		'y': person_age,
 	}
 	return Response(data)
 
 def chart_render(request, *args, **kwargs):
-	persons_by_user = Person.objects.filter(created_by_id=request.user)
 	return render(request, 'registrations/chart.html')
+
 
 @login_required(login_url='login')
 def form_delete(request, person_id):
-	# person = Person.objects.get(pk=person_id)
-	# person.delete()
-	# messages.success(request, ('Person been been deleted!'))
-	# return redirect('index')
-
 	current_user = request.user
 	if request.method == 'POST':
 		person = Person.objects.get(pk=person_id)
@@ -110,35 +102,3 @@ def form_render(request):
 #     else:
 #         print('ELSE..')
 
-
-# from snippets.serializers import UserSerializer
-
-# class data_rest(APIView):
-# 	authentication_classes = []
-# 	permission_classes = []
-
-# 	def get(self, request, format=None):
-# 		current_user = self.request.user
-# 		print(current_user)
-# 		# persons_by_user = Person.objects.filter(created_by_id=current_user)
-# 		persons = Person.objects.all()
-
-# 		df = pd.DataFrame(list(persons.values(
-# 			'name',
-# 			'duration', 
-# 			'created_at',
-# 			'updated_at',
-# 			'created_by_id',
-# 			)))		
-# 		df_grp = df.groupby('duration').count()
-# 		x = df_grp.index.values
-# 		y = df_grp.name.values
-
-# 		# x = []
-# 		# y = []
-
-# 		data = {
-# 				'x': x,
-# 				'y': y,
-# 		}
-# 		return Response(data)
